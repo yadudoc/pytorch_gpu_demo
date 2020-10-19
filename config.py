7e3fd931f2039024e629d801c896cb7cd53edbc6
Yadu Nand Babuji <yadudoc1729@gmail.com>
2020-10-19 11:56:25 -0500

    Updating config to gpu bind using map_gpu option

--- config.py
+++ config.py
@@ -8,12 +8,13 @@ from parsl.addresses import address_by_hostname
 
 # Configure options here:
 NODES_PER_JOB = 2
-GPUS_PER_NODE = 4 
+GPUS_PER_NODE = 4
 GPUS_PER_WORKER = 1
 
 # Do not modify:
 TOTAL_WORKERS = int((NODES_PER_JOB*GPUS_PER_NODE)/GPUS_PER_WORKER)
 WORKERS_PER_NODE = int(GPUS_PER_NODE / GPUS_PER_WORKER)
+GPU_MAP = ','.join([str(x) for x in range(1,TOTAL_WORKERS + 1)])
 
 config = Config(
     executors=[
@@ -26,14 +27,14 @@ config = Config(
                 nodes_per_block=NODES_PER_JOB,
                 init_blocks=1,
                 partition='geforce',
-                scheduler_options=f'#SBATCH --gres=gpu:{GPUS_PER_NODE}',
+                scheduler_options=f'#SBATCH --gpus-per-node=rtx2080ti:{GPUS_PER_NODE}',
                 # Launch 4 managers per node, each bound to 1 GPU
                 # This is a hack. We use hostname ; to terminate the srun command, and start our own
                 # DO NOT MODIFY unless you know what you are doing.
                 launcher=SrunLauncher(overrides=(f'hostname; srun --ntasks={TOTAL_WORKERS} '
                                                  f'--ntasks-per-node={WORKERS_PER_NODE} '
-                                                 f'--gpus-per-task={GPUS_PER_WORKER} '
-                                                 f'--gpu-bind=closest')
+                                                 f'--gpus-per-task=rtx2080ti:{GPUS_PER_WORKER} '
+                                                 f'--gpu-bind=map_gpu:{GPU_MAP}')
                 ),
                 walltime='01:00:00',
             ),
